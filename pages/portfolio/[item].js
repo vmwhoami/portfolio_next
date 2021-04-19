842adff4a0ac648ce076f3303093a743e687ff1f
vmwhoami <vmwhoami@gmail.com>
2021-04-19 17:51:02 +0300

    First commit

---
+++ pages/portfolio/[item].js
@@ -0,0 +1,14 @@
+import { useRouter } from "next/router"
+
+const PortfolioItem = () => {
+  const router = useRouter()
+  const pathname = router.query.item
+  console.log(pathname);
+  return (
+    <div>
+      <h1>This is a portfolio Item</h1>
+    </div>
+  )
+}
+
+export default PortfolioItem
---
+++ pages/portfolio/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+const Portfolio = () => {
+  return (
+    <div >
+      <Link href="/"> to home</Link>
+      <h1>Hello from portfolio page</h1>
+    </div >
+  )
+}
+
+export default Portfolio
