327eb36a01f7184718e331d380f388fe1c6852df
vmwhoami <vmwhoami@gmail.com>
2021-04-26 18:06:12 +0300

    Add the login page input fields and styling

--- pages/login.js
+++ pages/login.js
@@ -1,13 +1,34 @@
 import Heading from '../components/smallComponents/Heading';
-
+import { Lock } from '../components/Svgs'
 const Login = () => (
   <div className="container">
     <Heading
-      white="Login"
-      color="Page"
-      bg="Login"
+      white="Admin"
+      color="Login"
+      bg="Authenticate"
     />
+    <div className="login">
+      <form className="form">
+        <div className="form__container">
+          <input type="hidden" name="form-name" value="contact" />
 
+          <div className="login__input">
+            <input required placeholder="EMAIL" type="email" name="email" id="email" />
+          </div>
+
+          <div className="login__input">
+            <input required placeholder="PASSWORD" type="password" name="password" id="password" />
+          </div>
+
+        </div>
+        <button type="submit" className="mybutton">
+          <span className="mybutton__span">Login</span>
+          <i className="mybutton__icon">
+            <Lock />
+          </i>
+        </button>
+      </form>
+    </div>
   </div>
 );
 
