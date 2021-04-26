57ae53785009c10abda0d5f7c187eb8256bb5bc0
vmwhoami <vmwhoami@gmail.com>
2021-04-26 18:30:14 +0300

    Solve button styling bug

--- pages/login.js
+++ pages/login.js
@@ -1,35 +1,40 @@
 import Heading from '../components/smallComponents/Heading';
 import { Lock } from '../components/Svgs'
-const Login = () => (
-  <div className="container">
-    <Heading
-      white="Admin"
-      color="Login"
-      bg="Authenticate"
-    />
-    <div className="login">
-      <form className="form">
-        <div className="form__container">
-          <input type="hidden" name="form-name" value="contact" />
+const Login = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault()
 
-          <div className="login__input">
-            <input required placeholder="EMAIL" type="email" name="email" id="email" />
+  }
+  return (
+    <div className="container">
+      <Heading
+        white="Admin"
+        color="Login"
+        bg="Authenticate"
+      />
+      <div className="login">
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="form__container">
+            <input type="hidden" name="form-name" value="contact" />
+            <div className="login__input">
+              <input required placeholder="EMAIL" type="email" name="email" id="email" />
+            </div>
+
+            <div className="login__input">
+              <input required placeholder="PASSWORD" type="password" name="password" id="password" />
+            </div>
+
           </div>
-
-          <div className="login__input">
-            <input required placeholder="PASSWORD" type="password" name="password" id="password" />
-          </div>
-
-        </div>
-        <button type="submit" className="mybutton">
-          <span className="mybutton__span">Login</span>
-          <i className="mybutton__icon">
-            <Lock />
-          </i>
-        </button>
-      </form>
+          <button type="submit" className="mybutton">
+            <span className="mybutton__span">login</span>
+            <i className="mybutton__icon">
+              <Lock size={"size"} />
+            </i>
+          </button>
+        </form>
+      </div>
     </div>
-  </div>
-);
+  )
+};
 
 export default Login;
