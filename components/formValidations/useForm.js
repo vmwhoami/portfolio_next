f10f0f76f6235b694880969daa8ae5405013c73a
vmwhoami <vmwhoami@gmail.com>
2021-04-30 14:53:50 +0300

    Add form handler

--- components/formValidations/useForm.js
+++ components/formValidations/useForm.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export const useForm = (validation) => {
   const [values, setValues] = useState({
@@ -8,6 +8,7 @@ export const useForm = (validation) => {
     message: '',
   })
   const [errors, setErrors] = useState({})
+  const [sending, setSending] = useState(false)
 
   const handleChange = (e) => {
     const { name, value } = e.target
@@ -21,8 +22,11 @@ export const useForm = (validation) => {
   const handleSubmit = (e) => {
     e.preventDefault()
     setErrors(validation(values))
-    if (!errors) {
-      console.log("this will be sent");
+
+    if (Object.values(errors).length === 0) {
+      console.log("This is going to be sent");
+    } else {
+      console.log("This is not going to be sent");
     }
   }
 
