6f89cee46a80e50791e0ad851e589251cb2400cb
vmwhoami <vmwhoami@gmail.com>
2021-05-04 14:11:28 +0300

    Add script tag into head element

--- pages/_document.js
+++ pages/_document.js
@@ -8,16 +8,23 @@ class Mydocument extends Document {
       <Html lang="en">
         <Head>
           {/* was this working */}
-          <YMInitializer
-            accounts={[77676457]}
-            options={{
-              clickmap: true,
-              trackLinks: true,
-              accurateTrackBounce: true,
-              webvisor: true,
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `<script type="text/javascript" >
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+           
+              ym(77676457, "init", {
+                   clickmap:true,
+                   trackLinks:true,
+                   accurateTrackBounce:true,
+                   webvisor:true
+              });
+           </script>`
             }}
-            version="2"
           />
+
         </Head>
         <body>
           <Main />
