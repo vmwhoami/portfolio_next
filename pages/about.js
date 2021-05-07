8932d99c1bffba56a068a111749de305cf3ab580
vmwhoami <vmwhoami@gmail.com>
2021-05-07 17:14:03 +0300

    Add the basic info for the about page

--- pages/about.js
+++ pages/about.js
@@ -8,7 +8,11 @@ export const About = () => (
       color="me"
       bg="About"
     />
-    <Link href="/">Home</Link>
+    <p>Hi there as I already stated on my main page my name is Vitalie.</p><p>I'm a full-stack developer and I've spent the whole 2020 working with people across five different time zones in a Development Program called Microverse.</p>
+    <h2>Experience</h2>
+
+    <h2>Skills</h2>
+    <h2>Personal</h2>
   </div>
 );
 
