a9fa6ed2b40ebc1d29fda66b04710169ac153778
vmwhoami <vmwhoami@gmail.com>
2021-05-08 16:53:56 +0300

    Style the top of the about page

--- pages/about.js
+++ pages/about.js
@@ -7,22 +7,22 @@ export const About = () => (
       color="me"
       bg="About"
     />
-
     <div className="aboutp">
-      <div>
-        <p>Hi there as I already stated on my main page my name is Vitalie.</p><p>I'm a full-stack developer and I've spent the whole 2020 working with people across five different time zones in a Development Program called Microverse.</p>
 
-      </div>
-      <h2>Experience</h2>
-      <h3>Knoledge</h3>
+      <main>
+        <article>
+          <p> Hi there as I already stated on my main page my name is Vitalie.
+        </p>
+          <p>  I'm a full-stack developer and I've spent the whole 2020 working with people across five different time zones in a Development Program called Microverse.</p>
+        </article>
+
+        <div className="aboutimg">
+          <img src="/pictures/mainabout.jpg" alt="vitalieMelnic" />
+        </div>
+      </main>
 
 
-      <h2>Skills</h2>
 
-      <div>
-        <h2>Personal</h2>
-        <p>this part should contain images and videos about me dancing</p>
-      </div>
     </div>
   </div>
 
