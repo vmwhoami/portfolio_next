3f34a5b7228f8c8f3fe54121aa6d2e0daa9dc264
vmwhoami <vmwhoami@gmail.com>
2021-05-08 16:11:34 +0300

    Start styling the about page

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import Heading from '../components/smallComponents/Heading';
 
 export const About = () => (
@@ -8,12 +7,25 @@ export const About = () => (
       color="me"
       bg="About"
     />
-    <p>Hi there as I already stated on my main page my name is Vitalie.</p><p>I'm a full-stack developer and I've spent the whole 2020 working with people across five different time zones in a Development Program called Microverse.</p>
-    <h2>Experience</h2>
 
-    <h2>Skills</h2>
-    <h2>Personal</h2>
+    <div className="aboutp">
+      <div>
+        <p>Hi there as I already stated on my main page my name is Vitalie.</p><p>I'm a full-stack developer and I've spent the whole 2020 working with people across five different time zones in a Development Program called Microverse.</p>
+
+      </div>
+      <h2>Experience</h2>
+      <h3>Knoledge</h3>
+
+
+      <h2>Skills</h2>
+
+      <div>
+        <h2>Personal</h2>
+        <p>this part should contain images and videos about me dancing</p>
+      </div>
+    </div>
   </div>
+
 );
 
 export default About;
