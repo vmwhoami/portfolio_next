6cf86fbc9855254817ebfc9fcf2c92ed4bd5ef3e
Vitalie Melnic <vmwhoami@gmail.com>
2021-05-10 18:01:51 +0300

    Add hover effect to the info section

--- components/smallComponents/Info.js
+++ components/smallComponents/Info.js
@@ -3,13 +3,13 @@ import { Minus, Plus } from '../Svgs'
 const Info = ({ title, period, location, description }) => {
   const [info, showInfo] = useState(false)
   return (
-    <article className="info">
+    <article className="info" onClick={() => showInfo(!info)}>
       <header className="info__header">
         <div className="info__longinfo">
-          <button className="info__btn" onClick={() => showInfo(!info)}>
+          <button className="info__btn" >
             {info ? <Minus /> : <Plus />}
           </button>
-          <h3>{title}</h3>
+          <h4>{title}</h4>
         </div>
         <div className="info__shortinfo">
           <p className="info__period">{period}</p>
