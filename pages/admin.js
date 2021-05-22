201ad09d18dd03a3d805de6602800eb345616f3b
vmwhoami <vmwhoami@gmail.com>
2021-05-22 17:03:27 +0300

    Add admin page

---
+++ pages/admin.js
@@ -0,0 +1,18 @@
+import Heading from '../components/smallComponents/Heading';
+
+const AdminPanel = () => {
+  return (
+    <div className="container">
+      <Heading
+        white="Admin"
+        color="Login"
+        bg="Authenticate"
+      />
+      <div className="admin">
+
+      </div>
+    </div>
+  );
+};
+
+export default AdminPanel;
