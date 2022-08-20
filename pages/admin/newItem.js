3968bb5051612eeff09159f74bee8ea91ab2761a
vmwhoami <vitalie.m@travod.com>
2022-08-20 21:38:32 +0300

    update babel  core preset react

--- pages/admin/newItem.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const NewItem = () => (
-  <div>
-    <h2>This is the new item page</h2>
-  </div>
-);
-
-export default NewItem;
--- pages/api/feedback.js
+++
@@ -1,3 +0,0 @@
-const feedback = (req, res) => res.status(200).json({ hello: 'Hello in the casa mare' });
-
-export default feedback;
