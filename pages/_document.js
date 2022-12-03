afd4640d25372422f48d66b2e3aa52523d228ec3
vmwhoami <vitalie.m@travod.com>
2022-12-03 15:05:28 +0200

    Remove yandex tracking

--- pages/_document.js
+++ pages/_document.js
@@ -6,35 +6,7 @@ class Mydocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
-          {/* was this working */}
-          <script
-            type="text/javascript"
-            dangerouslySetInnerHTML={{
-              __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){
-                (m[i].a=m[i].a||[]).push(arguments)
-              };
-              m[i].l=1*new Date();
-              k=e.createElement(t),
-              a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-           
-              ym(72187387, "init", {
-                   clickmap:true,
-                   trackLinks:true,
-                   accurateTrackBounce:true,
-                   webvisor:true
-              });
-           </script>
-           <noscript>
-            <div>
-              <img src="https://mc.yandex.ru/watch/72187387" style="position:absolute; left:-9999px;" alt="" />
-            </div>
-           </noscript>`,
-            }}
-          />
-          <meta name="google-site-verification" content="UUZectgnSbjlwfqkJjJG8yZmsqc5vfv74JSKisGsa4E" />
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
